b84ad96c13f815eaed0aa70a6da333ddbb81b81f
Sergi <scsanz87@gmail.com>
2023-05-23 11:05:48 +0200

    "second commit"

---
+++ final-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="principal">
+        <h1>VAMOS A POR TODAS JODER</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
