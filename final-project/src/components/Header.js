83cbedecc8e9275cb77fc32456496ac5b644193e
Sergi <scsanz87@gmail.com>
2023-05-23 18:09:47 +0200

    Header.jsx + styles + icons + Intall svg incons

--- final-project/src/components/Header.js
+++ final-project/src/components/Header.js
@@ -1,22 +1,33 @@
-import React from 'react'
-
+import React from 'react';
+import "./Header.css";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { KeyboardArrowDown } from "@mui/icons-material"
 
 
 const Header = () => {
   return (
-    <div className='header'>Header
-        <h1>Image</h1>
+    <div className='header'>
+        <img src='/images/giphyLogo.png' alt='logo'/> 
         <div className='menu'>
             <h2>Reactions</h2>
             <h2>Entertaiment</h2>
             <h2>Sports</h2>
             <h2>Stickers</h2>
             <h2>Artists</h2>
-            <h2>MorevertIcon</h2>
+            <MoreVertIcon />
 
         </div>
-    
-    
+        <div className='button'>
+            <h2>Upload</h2>
+        </div>
+        <div className='button'>
+            <h2>Create</h2>
+        </div>
+        <div className='profile'>
+            <img src="/images/avatar.png" alt='avatar'/>
+            <h2>Sergio</h2>
+            <KeyboardArrowDown />
+        </div>
     </div>
   )
 }
