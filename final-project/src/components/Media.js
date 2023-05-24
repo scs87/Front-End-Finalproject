f72850375980e10e57ff03264a096bbb3ce154b4
Sergi <scsanz87@gmail.com>
2023-05-24 11:54:13 +0200

    Media.css Styled components

--- final-project/src/components/Media.js
+++ final-project/src/components/Media.js
@@ -6,26 +6,39 @@ const Media = () => {
         <div className='media'>
             <div className='row'>
                 <div className='row-header'>
+
                     <img src='/images/trending.svg' alt='Trending' />
                     <h1>Trending</h1>
                 </div>
+                <div className='trending-container'>
+                    <p>Content</p>
+                </div>
             </div>
             <div className='row'>
                 <div className='row-header'>
-                <img src='/images/artists.svg' alt='Artists' />
+                    <img src='/images/artists.svg' alt='Artists' />
                     <h1>Artists</h1>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='row-header'>
-                <img src='/images/clips.svg' alt='Clips' />
-                    <h1>Clips</h1>
+                <div className='artists-container'>
+                    <p>Content</p>
                 </div>
             </div>
             <div className='row'>
                 <div className='row-header'>
-                <img src='/images/stories.svg' alt='Stories' />
+                    <img src='/images/clips.svg' alt='Clips' />
+                    <h1>Clips</h1>
+                </div>
+                <div className='clips-container'>
+                    <p>Content</p>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='row-header'>
+                    <img src='/images/stories.svg' alt='Stories' />
                     <h1>Stories</h1>
+                </div>
+                <div className='stories-container'>
+                    <p>Content</p>
                 </div>
             </div>
         </div>
