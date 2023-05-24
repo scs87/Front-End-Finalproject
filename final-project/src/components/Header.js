affcbe0a6d81cae947dc119c3c38517996b374f0
Sergi <scsanz87@gmail.com>
2023-05-24 10:39:01 +0200

    header.js and header.css finished

--- final-project/src/components/Header.js
+++ final-project/src/components/Header.js
@@ -5,31 +5,55 @@ import { KeyboardArrowDown } from "@mui/icons-material"
 
 
 const Header = () => {
-  return (
-    <div className='header'>
-        <img src='/images/giphyLogo.png' alt='logo'/> 
-        <div className='menu'>
-            <h2>Reactions</h2>
-            <h2>Entertaiment</h2>
-            <h2>Sports</h2>
-            <h2>Stickers</h2>
-            <h2>Artists</h2>
-            <MoreVertIcon />
+    return (
+        <div className='header'>
+            <img src='/images/giphyLogo.png' alt='logo' />
+            <div className='menu'>
+                <div className='button-wrapper reactions'>
+                    <div className='menu-button hover-reactions'>
+                        <h2>Reactions</h2>
+                    </div>
+                </div>
+                <div className='button-wrapper entertaiment'>
+                    <div className='menu-button hover-entertaiment'>
+                        <h2>Entertaiment</h2>
+                    </div>
+                </div>
+                <div className='button-wrapper sports'>
+                    <div className='menu-button hover-sports'>
+                        <h2>Sports</h2>
+                    </div>
+                </div>
+                <div className='button-wrapper stickers'>
+                    <div className='menu-button hover-stickers'>
+                        <h2>Stickers</h2>
+                    </div>
+                </div>
+                <div className='button-wrapper artists'>
+                    <div className='menu-button hover-artists'>
+                        <h2>Artists</h2>
+                    </div>
+                </div>
+                <div className='button-wrapper moreverticon'>
+                    <div className='menu-button hover-moreverticon'>
+                        <MoreVertIcon />
+                    </div>
+                </div>
 
+            </div>
+            <div className='button'>
+                <h2>Upload</h2>
+            </div>
+            <div className='button'>
+                <h2>Create</h2>
+            </div>
+            <div className='profile'>
+                <img src="/images/avatar.png" alt='avatar' />
+                <h2>Sergio</h2>
+                <KeyboardArrowDown />
+            </div>
         </div>
-        <div className='button'>
-            <h2>Upload</h2>
-        </div>
-        <div className='button'>
-            <h2>Create</h2>
-        </div>
-        <div className='profile'>
-            <img src="/images/avatar.png" alt='avatar'/>
-            <h2>Sergio</h2>
-            <KeyboardArrowDown />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
